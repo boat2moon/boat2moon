855b5b49eafc3a7a89ec663b2e9e43ff4fa69826
boat2moon <73120427+boat2moon@users.noreply.github.com>
2026-02-03 13:49:51 +0800

    feat: remove RobotPrinter feature

--- src/components/RobotPrinter/menus/index.ts
+++
@@ -1,6 +0,0 @@
-// 菜单组件导出
-export { ActionMenu } from './ActionMenu';
-export { ActionButton } from './ActionButton';
-
-// 类型导出
-export type { ActionConfig } from './types';
--- src/components/RobotPrinter/robot/index.ts
+++
@@ -1,4 +0,0 @@
-// 机器人头部组件导出
-export { RobotHead } from './RobotHead';
-export { Eyes } from './Eyes';
-export { Antenna } from './Antenna';
