5218ba6b33105d3787b1d09df36bf9ac93ec7bfc
boat2moon <73120427+boat2moon@users.noreply.github.com>
2026-03-08 14:52:20 +0800

    chore: format yaml, badge component, and data file

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,7 +8,8 @@ const Badge = React.forwardRef<HTMLSpanElement, BadgeProps>(({ className, ...pro
     <span
       ref={ref}
       className={cn(
-        "inline-flex items-center rounded-full border border-transparent bg-zinc-200 px-3 py-1 text-xs font-medium text-zinc-800 transition-colors dark:bg-zinc-800 dark:text-zinc-200",
+        `inline-flex items-center rounded-full border border-transparent bg-zinc-200 px-3 py-1
+        text-xs font-medium text-zinc-800 transition-colors dark:bg-zinc-800 dark:text-zinc-200`,
         className,
       )}
       {...props}
